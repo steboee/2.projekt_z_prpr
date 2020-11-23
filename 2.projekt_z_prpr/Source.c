ee29aba95ed8385ea19db4f05f0255904ef6000c
Ctibor Kovalčík <ckkoval33@gmail.com>
2020-11-23 21:28:51 +0100

    VECERNY UPDATE

--- 2.projekt_z_prpr/Source.c
+++ 2.projekt_z_prpr/Source.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+/*
 
 typedef struct zoznam{ // zoznam --> nazov struktury
 	char meno[50];
@@ -175,6 +175,136 @@ int main() {
 		}
 		if (input == 'k') {
 			exit(1);
+		}
+	}
+}*/
+typedef struct zoznam { // zoznam --> nazov struktury
+	char meno[50];
+	char druh[30];
+	int vyska;
+	float vaha;
+	long long datum;
+	long long krmenie;
+	char meno_osetrovatela[50];
+	struct zoznam* dalsi;
+}ZOZNAM;  // TYP (nie»o ako int, float)
+
+
+
+void function_n(ZOZNAM**a,int *pocet) {
+	FILE* file;
+	file = fopen("zvierata.txt", "r");
+	if (file == NULL) {
+		printf(" Zaznamy neboli nacitane\n");
+		exit(1);
+	}
+	fseek(file, 0, SEEK_SET);
+	char buff[100];
+	int n = 0;
+	pocet = 0;
+		while (fgets(buff, sizeof(buff), file) != NULL) {
+			if (strcmp(buff, "$$$\n") == 0) {
+				pocet++;
+				n++;
+			}
+		}
+	
+	
+	
+
+	
+	ZOZNAM*my = malloc(n*sizeof(ZOZNAM));
+
+	fseek(file, 0, SEEK_SET);
+	for (int i = 0; i < n; i++) {
+		fgets(buff, sizeof(buff), file);
+
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		strcpy(my[i].meno, buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		strcpy(my[i].druh, buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		my[i].vyska = atoi(buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		my[i].vaha = atof(buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		my[i].datum = atoll(buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		my[i].krmenie = atoll(buff);
+
+		fgets(buff, sizeof(buff), file);
+		buff[strlen(buff) - 1] = '\0';
+		strcpy(my[i].meno_osetrovatela, buff);
+
+		my[i].dalsi = (*a);
+		(*a) = my;
+	}
+	
+	return;
+}
+
+void function_v(ZOZNAM*a,int pocet) {
+	for (int i = 0; i < pocet; i++) {
+		puts("");
+		printf("Meno : %s\n", a[i].meno);
+		printf("Druh : %s\n", a[i].druh);
+		printf("Vyska : %d\n", a[i].vyska);
+		printf("Vaha : %g\n", a[i].vaha);
+		printf("datum : %lld\n", a[i].datum);
+		printf("krmenie : %lld\n", a[i].krmenie);
+		printf("osetrovatel : %s\n", a[i].meno_osetrovatela);
+	}
+}
+
+void function_h(ZOZNAM* a,int pocet) {
+	printf("Nacitaj datum krmenia: ");
+	long long datumkrmenia;
+	scanf("%lld", &datumkrmenia);
+	for (int i = 0; i < pocet; i++) {
+		if (datumkrmenia == a[i].krmenie) {
+			printf("Meno : %s\n", a[i].meno);
+			printf("Druh : %s\n", a[i].druh);
+			printf("Vyska : %d\n", a[i].vyska);
+			printf("Vaha : %g\n", a[i].vaha);
+			printf("datum : %lld\n", a[i].datum);
+			printf("krmenie : %lld\n", a[i].krmenie);
+			printf("osetrovatel : %s\n", a[i].meno_osetrovatela);
+		}
+	}
+}
+int main()
+{
+	char input;
+	ZOZNAM *zvierata = NULL;
+	int pocet = 0;
+
+	while (scanf("%c", &input)) {
+		printf("%d",pocet);
+		if (input == 'n') {
+			function_n(&zvierata,&pocet);
+		}
+		if (input == 'v') {
+			function_v(zvierata,pocet);
+		}
+		if (input == 'h') {
+			function_h(zvierata,pocet);
+		}
+		if (input == 'k') {
+			printf("KONIEC!\n");
+			exit(1);
+			
 		}
 	}
 }
